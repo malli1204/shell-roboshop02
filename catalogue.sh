b88b7c7150fb0612a6dfa605edf6c787949a2df9
malli <mallikarjuna2314@gmail.com>
2025-08-14 18:27:43 +0530

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -14,7 +14,7 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
 STATUS=$(mongosh --host mongodb.malli12.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-if [ $STATUS -lt 0 ]
+if [ $? -lt 0 ]
 then
     mongosh --host mongodb.malli12.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
